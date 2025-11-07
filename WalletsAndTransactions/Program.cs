6d328a7f89670e4e542c93cc8f0dbbad9c6e01ff
yoloroy <slart2108@gmail.com>
2025-11-07 21:33:44 +0300

    :tada: Начало проекта

---
+++ WalletsAndTransactions/Program.cs
@@ -0,0 +1,70 @@
+﻿using WalletsAndTransactions;
+
+var exit = false;
+
+var commands = new (string Name, Action Invoke)[]
+{
+    ("Внести данные", () => {}),
+    ("Вывести данные", () => {}),
+    ("Отформатировать данные для заданного месяца", () => {}),
+    ("Вывести 3 самых больших траты для каждого кошелька для указанного месяца", () => {}),
+    ("Выйти", () => exit = true)
+};
+
+var commandsPrinter = new TablePrinter
+(
+    commands
+        .Select((command, i) => new[] { i.ToString(), command.Name })
+        .Prepend(["ID", "Действие"])
+        .ToArray()
+);
+
+do
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    commandsPrinter.Print();
+    Console.WriteLine();
+
+    try
+    {
+        commands[ReadInt()].Invoke();
+    }
+    catch (CancelException)
+    {
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("! Вы ошиблись при вводе ID команды");
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("! Команды под таким ID не существует, перепрочтите список");
+    }
+} while (!exit);
+
+Console.WriteLine("Прощайте");
+return;
+
+int ReadInt()
+{
+    Console.Write("> ");
+    var line = Console.ReadLine();
+    if (line == null)
+    {
+        throw new CancelException();
+    }
+
+    try
+    {
+        return Convert.ToInt32(line);
+    }
+    catch (FormatException)
+    {
+        throw new FormatException();
+    }
+}
+
+internal class CancelException : Exception;
